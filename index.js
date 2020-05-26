dea7f88e2bedd12edb31265ace2b73e3b6c851cf
Elaine Ho <44984774+elaine-ho@users.noreply.github.com>
2020-05-25 17:43:44 -0700

    Addeda compressor to telephone effect

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var effect = null;
+var context, effect, gain;
 
+window.onload=function(){
+    context = new AudioContext();
+    effect = null;
+    gain = context.createGain();
+}
 
 navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
-    const context = new AudioContext();
     const input = context.createMediaStreamSource(stream);
     var convolver = context.createConvolver();
     effect = telephone(context);
@@ -15,19 +19,26 @@ navigator.mediaDevices.getUserMedia({
 });
 
 
-function telephone(audioContext) {
-    var lp1 = audioContext.createBiquadFilter();
+function telephone() {
+    var lp1 = context.createBiquadFilter();
     lp1.type = "lowpass";
-    lp1.frequency.value = 2000.0;
-    var lp2 = audioContext.createBiquadFilter();
+    lp1.frequency.value = 1500.0;
+    var lp2 = context.createBiquadFilter();
     lp2.type = "lowpass";
     lp2.frequency.value = 2000.0;
-    var hp1 = audioContext.createBiquadFilter();
+    var hp1 = context.createBiquadFilter();
     hp1.type = "highpass";
     hp1.frequency.value = 500.0;
-    var hp2 = audioContext.createBiquadFilter();
+    var hp2 = context.createBiquadFilter();
     hp2.type = "highpass";
-    hp2.frequency.value = 500.0;
+    hp2.frequency.value = 1000.0;
+    var reverb = context.createConvolver();
+    var compressor = context.createDynamicsCompressor();
+    compressor.threshold.setValueAtTime(-50, context.currentTime);
+    gain = context.createGain();
+    reverb.connect(compressor);
+    gain.connect(compressor);
+    lp1.connect(gain);
     lp1.connect( hp1 );
     lp2.connect( hp1 );
     hp1.connect( hp2 );
