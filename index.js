91397af3a7b8887d0fdfd6ffede678a076400425
Elaine Ho <44984774+elaine-ho@users.noreply.github.com>
2020-05-29 20:55:48 -0700

    Added lagging effect

--- index.js
+++ index.js
@@ -10,11 +10,72 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     const input = context.createMediaStreamSource(stream);
-    plane(input); // change here for effect
+    lagging(input); // change here for effect
 }, error => {
     console.log(error);
 });
 
+//https://github.com/urtzurd/html-audio
+function lagging(input){
+    var grainSize = 8192;
+    var pitchRatio = 1.0;
+    pitchShifterProcessor = context.createScriptProcessor(grainSize, 1, 1);
+    pitchShifterProcessor.buffer = new Float32Array(grainSize * 2);
+    pitchShifterProcessor.grainWindow = hannWindow(grainSize);
+    pitchShifterProcessor.onaudioprocess = function (event) {
+        var inputData = event.inputBuffer.getChannelData(0);
+        var outputData = event.outputBuffer.getChannelData(0);
+        for (i = 0; i < inputData.length; i++) {
+            // Apply the window to the input buffer
+            inputData[i] *= this.grainWindow[i];
+            // Shift half of the buffer
+            this.buffer[i] = this.buffer[i + grainSize];
+            // Empty the buffer tail
+            this.buffer[i + grainSize] = 0.0;
+        }
+        // Calculate the pitch shifted grain re-sampling and looping the input
+        var grainData = new Float32Array(grainSize * 2);
+        for (var i = 0, j = 0.0;
+             i < grainSize;
+             i++, j += pitchRatio) {
+
+            var index = Math.floor(j) % grainSize;
+            var a = inputData[index];
+            var b = inputData[(index + 1) % grainSize];
+            grainData[i] += linearInterpolation(a, b, j % 1.0) * this.grainWindow[i];
+        }
+        // Copy the grain multiple times overlapping it
+        for (i = 0; i < grainSize; i += Math.round(grainSize)) {
+            for (j = 0; j <= grainSize; j++) {
+                this.buffer[i + j] += grainData[j];
+            }
+        }
+        // Output the first half of the buffer
+        for (i = 0; i < grainSize; i++) {
+            outputData[i] = this.buffer[i];
+        }
+    };
+    input.connect(pitchShifterProcessor);
+    var delay = context.createDelay(10);
+    pitchShifterProcessor.connect(delay);
+    var distortion = context.createWaveShaper();
+    distortion.curve = makeDistortionCurve(50);
+    delay.connect(distortion);
+    distortion.connect(gain);
+}
+
+function linearInterpolation(a, b, t) {
+    return a + (b - a) * t;
+}
+function hannWindow(length) {
+    var window = new Float32Array(length);
+    for (var i = 0; i < length; i++) {
+        window[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
+    }
+    return window;
+}
+
+// https://noisehack.com/generate-noise-web-audio-api/
 function plane(input){
     var bufferSize = 4096;
     var brownNoise = (function() {
@@ -31,7 +92,6 @@ function plane(input){
         }
         return node;
     })();
-    // https://noisehack.com/generate-noise-web-audio-api/
     brownGain = context.createGain();
     brownGain.gain.setValueAtTime(.1, context.currentTime);
     brownNoise.connect(brownGain);
